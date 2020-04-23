cc13061f53d468270f30a14979f36e18342ef235
Juan Diaz <juan.rod.diaz@gmail.com>
2020-04-23 19:00:36 +0200

    Stating with pod logic.

---
+++ Assets/Scripts/Character/Pod/PodController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PodControllerData
+{
+    public Transform Owner;
+}
+
+public class PodController : MonoBehaviour
+{
+    [Header("Y Movement")]
+    [SerializeField]
+    private float _verticalAmplitude = 0.25f;
+    [SerializeField]
+    private float _frecuency = 1.0f;
+    [Header("X/Z Movement")]
+
+    private Vector3 _initialPosition = Vector3.zero;
+
+    public void Initialize(PodControllerData data)
+    {
+
+    }
+
+    private void Update()
+    {
+        ApplyVerticalMovement();
+    }
+
+    public void Attack()
+    {
+    }
+
+    private void ApplyVerticalMovement()
+    {
+    }
+
+    private void ApplyHorizontalMovement()
+    {
+    }
+}
