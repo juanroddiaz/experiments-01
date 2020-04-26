a612e291b6c00706797cca8623d64a7a4c54cd0d
Juan Diaz <juan.rod.diaz@gmail.com>
2020-04-26 14:37:31 +0200

    Adding reach colliders.

---
+++ Assets/Scripts/Scene/CollisionTriggerLogic.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionTriggerLogic : MonoBehaviour
+{
+    private Action _onTriggerEnterAction;
+    private Action _onTriggerExitAction;
+
+    public void Initialize(Action action)
+    {
+        _onTriggerEnterAction = action;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("On trigger enter: other " + other.name + ", collider: " + gameObject.name);
+        _onTriggerEnterAction?.Invoke();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log("On trigger exit: other " + other.name + ", collider: " + gameObject.name);
+        _onTriggerExitAction?.Invoke();
+    }
+}
